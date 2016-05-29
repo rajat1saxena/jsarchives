68d8ff3e08c5b226cd6d6342fb12178625f365cc
Rajat Saxena <rajat@raynstudios.in>
2016-05-29 22:41:50 +0530

    functions_fun updated with more patterns

--- functions_fun.js
+++ functions_fun.js
@@ -15,7 +15,7 @@ var app = {};
 app.times_called = 0;
 app.value = 1;
 
-// Method invocation pattern
+// 1. Method invocation pattern
 app.add = function(a, b) {
 	// In this method 'this' refers to the object
 	// this function is a part of.
@@ -27,22 +27,56 @@ app.add = function(a, b) {
 	return a + b;
 };
 
-// Function invocation pattern
-app.double = function() {
-	// To refer to the enclosing object
-	var that = this;
+// 2. Function invocation pattern
+//app.double = function() {
+//	// To refer to the enclosing object
+//	var that = this;
+//
+//	var helper = function() {
+//		that.value = adder(that.value, that.value);	
+//	};
+//
+//	helper();
+//};
 
-	var helper = function() {
-		that.value = adder(that.value, that.value);	
-	};
-
-	helper();
+// 3. Constructor pattern
+app.Foo = function(y) {
+	this.y = y;
 };
+app.Foo.prototype.x = 10;
+app.Foo.prototype.fakeMethod = function() {
+	return this.w * 2;
+};
+app.Foo.prototype.calculate = function(z) {
+	return this.x + this.y + z;
+};
+app.foo1 = new app.Foo(20);
+console.log('Calculate: ' + app.foo1.calculate(10));
+
+// 4. Apply Invocation pattern
+app.fakeObject = {
+	w: 2
+};
+// fakeObject does not inherit from app.Foo.prototype,
+// but we can invoke the fakeMethod on fakeObject even
+// though fakeObject does not have a fakeMethod method.
+console.log('Apply invocation: ' + app.Foo.prototype.fakeMethod.apply(app.fakeObject));
+
+// 5. Arguments
+app.addAll = function() {
+	var i, sum = 0;
+	for(i=0; i < arguments.length; i++) {
+		sum += arguments[i];
+	}
+	return sum;
+};
+console.log('addAll: ' + app.addAll(1,2,3));
+
 
 // Call the add function
 app.add(6, 5);
-// Call the double function
-app.double();
+// Call the double function ; Uncomment to use Func. Inv. pattern 
+//app.double();
 console.log(app.value);
 
 // Export app
