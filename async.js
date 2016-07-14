c7444289ba98c263e1962e35b2da768cf4b9d7be
Rajat Saxena <rajat@raynstudios.in>
2016-07-14 19:33:05 +0530

    Http client program added to lyn

---
+++ async.js
@@ -0,0 +1,18 @@
+// This program demonstrates that async commands don't block the rest of
+// the file from executing, like in other sync based executions
+var fs = require('fs');
+var myNumber = undefined;
+
+function addOneMore() {
+	fs.readFile('number.txt', function doneReading(err, fileContent) {
+		myNumber = parseInt(fileContent);
+		myNumber++;
+
+		// Print the number
+		console.log(myNumber);
+	});
+}
+
+addOneMore();
+
+console.log(myNumber);
