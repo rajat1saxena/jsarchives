1a15785dba07ad5e1929b95acb7c7e163e0e4b31
rajat saxena <rajat@raynstudios.in>
2016-12-29 13:03:17 -0500

    Counting sort added to algos

---
+++ algos/countingsort.js
@@ -0,0 +1,35 @@
+// Counting sort, based on CLRS. 
+// Complexity: O(n)
+var counting = function (arr) {
+	var that = {};
+
+	var sort = function () {
+		var c = {};
+		var i;
+		var b = [];
+		for (i = 0; i<10; i++) {
+			c[i] = 0;
+		}
+		for (i = 0; i < arr.length; i++) {
+			c[arr[i]] += 1;
+		}
+		for (i = 0; i < 9; i++) {
+			c[i+1] = c[i] + c[i+1];	
+		}
+		for (i = 0; i < arr.length; i++) {
+			b[c[arr[i]]] = arr[i];
+			c[arr[i]] -= 1;
+		}
+		
+		return b;
+	}
+
+	that.sort = function () {
+		return sort ();
+	}
+
+	return that;
+}
+
+var counter= counting ([7,8,8,7,8,8,8,9,0]);
+console.log(counter.sort());
